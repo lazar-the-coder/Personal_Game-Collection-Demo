45eea27572e7a3f8c915a12829f4d1b92a9b725b
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-05-08 16:31:17 -0500

    V1
    Really need to fix the colors but right now lets get the systems

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+// Gladlibs
+let needed
+let story =     `It was $noun1 day at school, 
+                and $propNoun1 was super $adj1 for lunch. 
+                But when she went outside to eat, 
+                a $noun2 stole her $noun1! 
+                $propNoun1 chased the $noun2 all over school. 
+                She $verb1, $verb2, and $verb3 through the playground. 
+                Then she tripped on her $noun2 and the $noun2 escaped! 
+                Luckily, $propNoun1’s friends were willing to share their ${ noun1 } with her.`;
+const word = {
+    text: 
+};
+
+function replaceAll()
+story = story.replace('burger', 'sock');
+
+const $storyElem = $(`<p>${story}</p>`);
+$('main .wrapper').html($storyElem);
