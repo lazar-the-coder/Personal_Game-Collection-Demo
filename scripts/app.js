4dbcf09cdaaefb1c4e7c1ae6f44cf750c46cb6ba
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-05-10 10:49:27 -0500

    Glad Libs down
    Still needs touching up, may convert to react.

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,111 @@
-// Gladlibs
-let needed
-let story =     `It was $noun1 day at school, 
-                and $propNoun1 was super $adj1 for lunch. 
-                But when she went outside to eat, 
-                a $noun2 stole her $noun1! 
-                $propNoun1 chased the $noun2 all over school. 
-                She $verb1, $verb2, and $verb3 through the playground. 
-                Then she tripped on her $noun2 and the $noun2 escaped! 
-                Luckily, $propNoun1’s friends were willing to share their ${ noun1 } with her.`;
-const word = {
-    text: 
-};
+// Document setup
+gameListHolder = document.getElementById('game-list');
+gamesList = ['Glad Libs', 'Tic-Tac-Toe'];
 
-function replaceAll()
-story = story.replace('burger', 'sock');
+gameListHolder.addEventListener("click", (event) => {
+    if (event.target.nodeName === "A") {
+        runGames(event.target.textContent)
+    }
+});
 
-const $storyElem = $(`<p>${story}</p>`);
-$('main .wrapper').html($storyElem);
+for (game of gamesList) {
+    gameListHolder.innerHTML += (`<li><a>${game}</a></li>`);
+}
+
+function runGames(game) {
+    switch (game) {
+        case 'Glad Libs':
+            gladLibs();
+            break;
+        case 'Tic-Tac-Toe':
+            break;
+        default:
+            break;
+    }
+}
+
+let story = ''
+
+const wordDic = {}
+
+// Glad Libs Start
+//Make the form to enter the words.
+function gladLibs() {
+
+
+    story =     `It was $%noun1$ day at school, 
+                    and $%propNoun1$ was super $%adj1$ for lunch. 
+                    But when she went outside to eat, 
+                    a $%noun2$ stole her $%noun1$! 
+                    $%propNoun1$ chased the $%noun2$ all over school. 
+                    She $%verb1$, $%verb2$, and $%verb3$ through the playground. 
+                    Then she tripped on her $%noun2$ and the $%noun2$ escaped! 
+                    Luckily, $%propNoun1$’s friends were willing to share their $%noun1$ with her.`;
+    printForm()
+}
+
+class StoryTemplate {
+    constructor (text) {
+        this.text = text;
+    }
+
+    getWords() {
+        let carved = this.text.split('$');
+        let carvedWords = [];
+        for (let word of carved) {
+            if (word.charAt(0) === '%') {
+                carvedWords.push(word);
+            }
+        }
+        let allWords = [...new Set(carvedWords)];
+        return [carved, allWords];
+    }
+
+    giveStory(dic) {
+        let storyParts = this.getWords();
+        let storyFull = storyParts[0].join('');
+        let words = storyParts[1];
+        let endStory = storyFull;
+        for (let word of words) {
+            endStory = endStory.replaceAll(word, dic[word]);
+        }
+        return endStory;
+    }
+}
+
+function printForm() {
+    newStory = new StoryTemplate(story);
+    const theStory = newStory.getWords();
+    document.getElementById('body').innerHTML = '<form></form>';
+    const form = document.getElementsByTagName('form')[0];
+    for (word of theStory[1]) {
+        form.innerHTML +=   `<div>
+                                <label for="${word}">${word.slice(1)}</label><br />
+                                <input type="text" name="noun" id="${word}">
+                            </div>`
+    }
+    form.innerHTML += `<input id="submitter" type="submit" value="Get Glad!"></input>`
+    form.addEventListener("click", (event) => {
+        if (event.target.getAttribute('id') === 'submitter') {
+            submitInfo()
+        }
+    });
+}
+
+function submitInfo() {
+    fieldData = document.getElementsByTagName('input');
+    fieldKey = newStory.getWords();
+    for (let i = 0; i < fieldKey[1].length; i++) {
+        wordDic[fieldKey[1][i]] = fieldData[i].value;
+    }
+    console.log(wordDic);
+    printStory()
+}
+
+function printStory() {
+    const theStory = newStory.giveStory(wordDic);
+    document.getElementById('body').innerHTML = `<p>${theStory}</p>`;
+}
+// Glad Libs End
+
+// Tic Tac Toe
